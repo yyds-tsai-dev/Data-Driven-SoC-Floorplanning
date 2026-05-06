db35d0c593ce480c80a88f0bd92c68c24d7ea539
yyds-tsai-dev <0979030@gmail.com>
2026-05-06 17:08:19 +0800

    add: new contest problem pdf

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 git submodule update --init --recursive
 curl -LsSf https://astral.sh/uv/install.sh | sh
 uv venv
-uv pip install -r FloorSet/iccad2026contest/requirements.txt
+uv pip install -r requirements.txt
 mkdir -p test
 cp FloorSet/iccad2026contest/optimizer_template.py test/my_optimizer.py
 uv run FloorSet/iccad2026contest/iccad2026_evaluate.py --evaluate test/my_optimizer.py --test-id 0 --verbose
