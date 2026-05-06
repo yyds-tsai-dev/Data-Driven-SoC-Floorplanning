dd708ecb358493c7826c836fa0f8100b3a4fc03c
yyds-tsai-dev <chengan81.ii14@nycu.edu.tw>
2026-05-06 16:21:12 +0800

    update: install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
-
+git submodule update --init --recursive
 curl -LsSf https://astral.sh/uv/install.sh | sh
+uv venv
+uv pip install -r FloorSet/iccad2026contest/requirements.txt
 mkdir -p test
 cp FloorSet/iccad2026contest/optimizer_template.py test/my_optimizer.py
-uv run --link-mode=copy FloorSet/iccad2026contest/iccad2026_evaluate.py --evaluate test/my_optimizer.py --test-id 0 --verbose
+uv run FloorSet/iccad2026contest/iccad2026_evaluate.py --evaluate test/my_optimizer.py --test-id 0 --verbose
