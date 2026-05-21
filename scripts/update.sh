ce06220f7ac1da6dff1abebee1347a1172f74655
yyds-tsai-dev <chengan81.ii14@nycu.edu.tw>
2026-05-21 14:58:16 +0800

    feat: update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,19 @@
 
 COMMIT_MSG="${1:-$(date +"%Y-%m-%d %H:%M:%S")}"
 REMOTE="${2:-origin}"
-BRANCH="${3:-v5-graph-transformer-encoder}"
+BRANCH="${3:-$(git branch --show-current)}"
+
+git ls-remote --exit-code --heads "$REMOTE" "$BRANCH" >/dev/null
+REMOTE_BRANCH_STATUS=$?
+
+if [ "$REMOTE_BRANCH_STATUS" -eq 0 ]; then
+  git pull --rebase "$REMOTE" "$BRANCH"
+elif [ "$REMOTE_BRANCH_STATUS" -eq 2 ]; then
+  echo "Remote branch $REMOTE/$BRANCH does not exist yet; skipping pull."
+else
+  echo "Unable to check remote branch $REMOTE/$BRANCH."
+  exit "$REMOTE_BRANCH_STATUS"
+fi
 
 git fetch
 git pull "$REMOTE" "$BRANCH"
